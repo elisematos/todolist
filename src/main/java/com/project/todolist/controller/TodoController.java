c077e85c2a785241e56d5dde5a956bb6c0d82cc7
ematos <mme.matos.elise@gmail.com>
2022-01-04 23:22:21 +0100

    gna

--- src/main/java/com/project/todolist/controller/TodoController.java
+++ src/main/java/com/project/todolist/controller/TodoController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("to")
+@RequestMapping("/v1/lists/")
 public class TodoController {
 
     private final TodoService todoService;
